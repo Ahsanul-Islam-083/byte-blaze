381da1fa039faab74c793c3a7e14a1c9ecb09780
Ahsanul-Islam-083 <ahsanulislam083@gmail.com>
2024-03-25 01:17:15 +0600

    added blog section

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import Homepage from './pages/Homepage.jsx'
 import Blogs from './pages/Blogs.jsx'
 import Bookmarks from './pages/Bookmarks.jsx'
 import MainLayouts from './layout/MainLayouts.jsx'
+import Blog from './pages/Blog.jsx'
 
 
 
@@ -22,7 +23,13 @@ const router = createBrowserRouter([
       },
       {
         path:'/blogs',
-        element:<Blogs></Blogs>
+        element:<Blogs></Blogs>,
+        loader:()=>fetch('https://dev.to/api/articles/latest?per_page=20&top=7')
+      },
+      {
+        path:'/blog/:id',
+        element:<Blog></Blog>,
+        loader:({params})=>fetch(`https://dev.to/api/articles/${params.id}`)
       },
       {
         path:'/bookmarks',
